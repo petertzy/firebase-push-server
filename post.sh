6fe3b9dbf189fa52d3349cb27b9414b96bec77cc
petertzy <obitzy@gmx.de>
2025-03-21 11:14:18 +0100

    Upadate

--- post.sh
+++ post.sh
@@ -1,4 +1,4 @@
-curl -X POST http://localhost:3000/send-notification \
+curl -X POST https://firebase-push-server.onrender.com/send-notification \
 -H "Content-Type: application/json" \
 -d '{
     "token": "fpuo18-tR-eHh_x0n3qU1i:APA91bERzv0carDR0yBtvSXpAcUJn5-XPcpsy4QC8pw4o-gtWkut56Qwx-Vpmcd0oNrvOwdcK7kSWhqm94t3rhW9qraiSi5f8mc-xjYs7Bs21imnb_LnoUM",
